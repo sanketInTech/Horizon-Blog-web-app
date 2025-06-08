24b5c66e12384bfc8af5c5ccb07dcdaf3fee662b
sanketInTech <dsanket034@gmail.com>
2025-06-08 18:36:27 +0530

    Update API URLs to use environment variable

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -27,7 +27,7 @@ const Register = ({ onClose, onSwitchToLogin }) => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
